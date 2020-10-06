940de8b3e1d207f15fa55d456ac4530a95fd16cf
naneri <ktnaneri@gmail.com>
2020-10-06 23:43:23 +0600

    added more stuff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,13 @@
-@extends('adminlte::page')
+@extends('layouts._app')
 
-@section('title', 'Dashboard')
+@section('title', 'Home')
 
 @section('content_header')
     <h1>Dashboard</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div id="app">
+        <p>Welcome to this beautiful admin panel.</p>
+    </div>
 @stop
