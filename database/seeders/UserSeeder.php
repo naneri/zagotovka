850f399b29d7330e284015503a4b2eedfbae4393
naneri <ktnaneri@gmail.com>
2021-01-06 17:11:28 +0600

    upgraded to laravel 8

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,12 +1,14 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
