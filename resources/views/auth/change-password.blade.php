af7851c9db1062ed33c8b30882427ada5e999927
naneri <ktnaneri@gmail.com>
2021-01-07 13:34:13 +0600

    added field colors

--- resources/views/auth/change-password.blade.php
+++ resources/views/auth/change-password.blade.php
@@ -15,17 +15,17 @@
                     <form action="{{route('update-password')}}" method="POST">
                         {{csrf_field()}}
                         <div class="form-group">
-                            <label for="password">password</label>
-                            <input type="password" id="password" class="form-control" name="password" value="{{old('password')}}">
-                            @if ($errors->has('password'))
-                                <span class="help-block">
+                            <label for="password">Password</label>
+                            <input type="password" id="password" class="form-control @if($errors->has('password'))is-invalid @endif" name="password" value="{{old('password')}}">
+                            @if($errors->has('password'))
+                            <span class="help-block">
                                 <strong class="text-red">{{ $errors->first('password') }}</strong>
                             </span>
                             @endif
                         </div>
                         <div class="form-group">
-                            <label for="password_confirmation">password_confirmation</label>
-                            <input type="password" id="password_confirmation" class="form-control" name="password_confirmation" value="{{old('password_confirmation')}}">
+                            <label for="password_confirmation">Password Confirmation</label>
+                            <input type="password" id="password_confirmation" class="form-control @if($errors->has('password_confirmation'))is-invalid @endif" name="password_confirmation" value="{{old('password_confirmation')}}">
                             @if ($errors->has('password_confirmation'))
                                 <span class="help-block">
                                 <strong class="text-red">{{ $errors->first('password_confirmation') }}</strong>
